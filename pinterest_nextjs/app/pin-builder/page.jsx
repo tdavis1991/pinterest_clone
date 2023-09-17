9a112f84ec7a1b88af414a27e51024be4cf03e82
Tevin_Davis <tdavis.070991@gmail.com>
2023-09-17 17:48:40 -0400

    Working on being able to add pins

--- pinterest_nextjs/app/pin-builder/page.jsx
+++ pinterest_nextjs/app/pin-builder/page.jsx
@@ -1,8 +1,16 @@
+"use client"
+
+import { BsThreeDots } from "react-icons/bs";
+
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">page</div>
+    <div className="flex min-h-screen flex-col items-center justify-center shadow-md p-24">
+      <div>
+        <BsThreeDots />
+      </div>
+    </div>
   )
 }
 
