887d7e1d1149bd167728a74a09b164651ebeba2b
Tevin_Davis <tdavis.070991@gmail.com>
2023-11-12 17:59:09 -0500

    Working on Pin details page

---
+++ pinterest_nextjs/app/api/user/[id]/route.js
@@ -0,0 +1,17 @@
+import { connectToDB } from "@/utils/database";
+import User from "@/models/user";
+
+export const GET = async (req, { params }) => {
+
+  try {
+    await connectToDB();
+
+    const existingUser = await User.findById(params.id)
+
+    console.log(existingUser, 'EXISTING USER')
+
+    return new Response(JSON.stringify(existingUser), { status: 201 })
+  } catch (error) {
+    return new Response('Failed to fetch user', { status: 500 })
+  }
+} 
--- pinterest_nextjs/app/pin/[...id]/page.jsx
+++ pinterest_nextjs/app/pin/[...id]/page.jsx
@@ -2,13 +2,29 @@
 
 import { useState, useEffect } from "react";
 import { CldImage } from 'next-cloudinary';
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 const page = ({ params }) => {
   const [pin, setPin] = useState({});
+  const [user, setUser] = useState({});
+  const { data: session } = useSession();
 
   const pinId = params.id
 
-  console.log(pin, 'PIN')
+  console.log(session)
+
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch(`/api/user/${session?.user?.id}`);
+      const data = await res.json();
+      console.log(data, 'USER')
+      setUser(data) 
+    }
+
+    if(session?.user) fetchUser();
+  }, [])
 
 
   useEffect(() => {
@@ -24,21 +40,42 @@ const page = ({ params }) => {
   
 
   return (
-    <div className="w-11/12 mt-5 gap-5 flex justify-center items-center">
-      <div>
-        <CldImage 
-          width={1000}
-          height={1000}
-          src={pin.imageUrl}
-          alt={pin.description}
-        />
-      </div>
-      <div className="flex flex-col">
-        <h1>{pin.title}</h1>
-        <div className="flex gap-3">
-          <button className="bg-gray-400 rounded-full px-5 py-2 text-white">View</button>
-          <button className="cta_btn rounded-full px-5 py-2 text-white">Save</button>
+    <div className="w-11/12 h-screen flex flex-col justify-center items-center">
+      <div className="rounded-xl shadow-xl flex w-2/3 border-gray-200 border">
+        <div>
+          <CldImage 
+            width={700}
+            height={700}
+            src={pin.imageUrl}
+            alt={pin.description}
+          />
         </div>
+        <div className="flex flex-col flex-1 items-center">
+          <div className="flex m-3">
+            {session?.user &&
+                <select className=' px-1 py-2'>
+                  <option>Create Board</option>
+                  {user?.boards?.map((board, i) => (
+                    <select key={i} value={board?.name?.toLowerCase()}></select>
+                  ))}
+                </select>
+              }
+              <button className="cta_btn rounded-full px-5 py-2 text-white">Save</button>
+          </div>
+          <h1>{pin?.title}</h1>
+          <div className="flex my-3 gap-5">
+            <Image 
+              src={session?.user.image}
+              alt="Profile picture"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <h2>{session?.user.name}</h2>
+            
+          </div>
+        </div>
+
       </div>
     </div>
   )
